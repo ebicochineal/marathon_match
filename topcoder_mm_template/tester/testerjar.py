a1c0ebd8b0249970cc4f0a62dd55ed0d87449bee
ebicochineal <yz94588@gmail.com>
2019-06-02 17:22:59 +0900

    fix

--- topcoder_mm_template/tester/testerjar.py
+++ topcoder_mm_template/tester/testerjar.py
@@ -2,35 +2,37 @@
 import os
 import sys
 
+# testerjar.py dir path
+cd = os.path.abspath(os.path.dirname(__file__)).replace('\\', '/') + '/'
+
 # javapathを通す
 javapath = ''
-with open('./javapath.ini', 'r') as f:
+with open(cd + 'javapath.ini', 'r') as f:
     for i in f.readlines():
         if not i.startswith('//') : javapath = i
 sp = ':;'['win' in sys.platform and 'darwin' != sys.platform]
 if not javapath in os.environ['PATH'] : os.environ['PATH'] += sp + javapath
 
-# testerjar.py dir path
-cd = os.path.abspath(os.path.dirname(__file__)).replace('\\', '/')
+
 
 # javaファイル
 target = ''
 for i in os.listdir(cd):
     if 'Vis.java' in i:
-        print(i)
-        target += i
+        print(cd + i)
+        target = cd + i
 
 # classname main
 classname = os.path.basename(target).split('.')[0]
 
 # class file dir
-dir = cd + '/build'
+dir = cd + 'build'
 
 # manifest path
-manifest = dir + '/manifest'
+manifest = dir + 'manifest'
 
 # tester.jar path
-tester = cd + '/tester.jar'
+tester = cd + 'tester.jar'
 
 # mkdir
 if not os.path.exists(dir) : os.mkdir(dir)
