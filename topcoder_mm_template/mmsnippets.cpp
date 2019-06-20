631b787269f8a19bde399413768b98cb50e4e2eb
ebicochineal <yz94588@gmail.com>
2019-06-21 00:41:20 +0900

    fix

--- topcoder_mm_template/mmsnippets.cpp
+++ topcoder_mm_template/mmsnippets.cpp
@@ -74,13 +74,24 @@ class StopWatch {
 public:
     int starts;
     int startm;
+    int tstarts = 0;
+    int tstartm = 0;
     struct timeb timebuffer;
     StopWatch () {
         ftime(&this->timebuffer);
         this->starts = this->timebuffer.time;
         this->startm = this->timebuffer.millitm;
     }
-    
+    inline void stop () {
+        ftime(&this->timebuffer);
+        this->tstarts = this->timebuffer.time;
+        this->tstartm = this->timebuffer.millitm;
+    }
+    inline void resume () {
+        ftime(&this->timebuffer);
+        this->starts += this->timebuffer.time - this->tstarts;
+        this->startm += this->timebuffer.millitm - this->tstartm;
+    }
     inline int get_milli_time () {
         ftime(&this->timebuffer);
         return (this->timebuffer.time - this->starts) * 1000 + (this->timebuffer.millitm - this->startm);
