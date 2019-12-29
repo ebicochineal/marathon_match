0af079600bfd311bafae519c72ffae004fcccf4c
ebicochineal <yz94588@gmail.com>
2019-12-29 23:35:32 +0900

    fix

--- atcoder_rco_marathon_match_template/interactive/a.cpp
+++ atcoder_rco_marathon_match_template/interactive/a.cpp
@@ -1,25 +1,26 @@
-#include <algorithm>
-#include <cstdlib>
-#include <iostream>
-#include <map>
-#include <sstream>
-#include <vector>
-#include <array>
-#include <set>
-#include <random>
+#include <cmath>
 #include <string>
+#include <array>
+#include <vector>
+#include <cstdlib>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <limits>
+#include <queue>
+#include <set>
 #include <unordered_set>
+#include <map>
 #include <unordered_map>
 #include <time.h>
 #include <sys/timeb.h>
-
 using namespace std;
 
 template<class F, class S> string in_v_to_str (const pair<F, S> v);
 template<class F, class S> string v_to_str (const pair<F, S> v);
 string in_v_to_str (const char v) { return "'" + string{v} + "'"; }
-string in_v_to_str (const char* v) { return '"' + v + '"'; }
-string in_v_to_str (const string v) { return '"' + v + '"'; }
+string in_v_to_str (const char* v) { return "\"" + string(v) + "\""; }
+string in_v_to_str (const string v) { return "\"" + v + "\""; }
 template<class T> string in_v_to_str (const T v) { stringstream ss; ss << v; return ss.str(); }
 template<class T> string v_to_str (const T v) { stringstream ss; ss << v; return ss.str(); }
 template<class T, size_t N> string v_to_str (const array<T, N>& v) { stringstream ss; if (v.size() > 0) { ss << "["; for (int i = 0; i < v.size() - 1; ++i) { ss << in_v_to_str(v[i]) << ", "; } ss << in_v_to_str(v[v.size() - 1]) << "]"; } else { ss << "[]"; } return ss.str(); }
@@ -46,6 +47,30 @@ template<typename F, typename... R> void fdebug (const F& f, const R& ...r) {
     ss << v_to_str(f); if (sizeof...(r) > 0) { ss << " " << print(r...); }
     cerr << "<cerrfile>" << ss.str() << "</cerrfile>" << endl;
 }
+
+struct e512pos {
+public:
+    int x;
+    int y;
+    e512pos () { this->x = 0; this->y = 0; }
+    e512pos (int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+    e512pos operator + (const e512pos& t) { return e512pos(this->x + t.x, this->y + t.y); }
+    e512pos operator - (const e512pos& t) { return e512pos(this->x - t.x, this->y - t.y); }
+    bool operator == (const e512pos& t) const { return this->x == t.x && this->y == t.y; }
+};
+namespace std {
+    template <> class hash<e512pos> {
+    public:
+        size_t operator()(const e512pos& t) const{ return hash<int>()(t.x<<16) | hash<int>()(t.y); }
+    };
+}
+ostream& operator << (ostream& os, const e512pos& p) {
+    os << "(" << p.x << ", " << p.y << ")";
+    return os;
+};
 
 class StopWatch {
 public:
@@ -75,32 +100,159 @@ public:
     }
 };
 
-struct e512pos {
-public:
-    int x;
-    int y;
-    e512pos () { this->x = 0; this->y = 0; }
-    e512pos (int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
-    e512pos operator + (const e512pos& t) { return e512pos(this->x + t.x, this->y + t.y); }
-    e512pos operator - (const e512pos& t) { return e512pos(this->x - t.x, this->y - t.y); }
-    bool operator == (const e512pos& t) const { return this->x == t.x && this->y == t.y; }
-};
-
-namespace std {
-    template <> class hash<e512pos> {
-    public:
-        size_t operator()(const e512pos& t) const{ return hash<int>()(t.x<<16) | hash<int>()(t.y); }
-    };
-}
-
 inline uint32_t xrnd() {
     static uint32_t y = 2463534242;
     y = y ^ (y << 13);
     y = y ^ (y >> 17);
     return y = y ^ (y << 5);
+}
+
+struct Edge {
+public:
+    int a, b, cost;
+    Edge () {
+        this->a = 0;
+        this->b = 0;
+        this->cost = 1000000;
+    }
+    Edge (int a, int b, int cost) {
+        this->a = a;
+        this->b = b;
+        this->cost = cost;
+    }
+    bool operator == (const Edge& t) const { return this->a == t.a && this->b == t.b; }
+};
+namespace std {
+    template <> class hash<Edge> {
+    public:
+        size_t operator()(const Edge& t) const{ return hash<int>()(t.a<<16) | hash<int>()(t.b); }
+    };
+}
+
+class GraphDijkstra {
+private:
+    class Node {
+    public:
+        int cost;
+        int index;
+        Node (int cost, int index) {
+            this->cost = cost;
+            this->index = index;
+        }
+        bool operator > (const Node& t) const { return this->cost > t.cost; }
+        bool operator < (const Node& t) const { return this->cost < t.cost; }
+    };
+    int tmp = -1;
+    vector<int> prevs;
+    vector<int> costs;
+    priority_queue<Node, vector<Node>, greater<Node> > que;
+    vector< vector< pair<int, int> > > edgecost;
+public:
+    int n;
+    vector<int> path;
+    vector<int> rpath;
+    int pathcost;
+    
+    GraphDijkstra () {}
+    
+    GraphDijkstra (int n, vector<Edge> edges, bool undir = false) {
+        this->pathcost = 0;
+        this->path.clear();
+        this->n = n;
+        this->edgecost = vector< vector< pair<int, int> > >(this->n, vector< pair<int, int> >());
+        if (undir) {// undirected
+            for (auto&& i : edges) {
+                this->edgecost[i.a].emplace_back(i.b, i.cost);
+                this->edgecost[i.b].emplace_back(i.a, i.cost);
+            }
+        } else {// directed
+            for (auto&& i : edges) {
+                this->edgecost[i.a].emplace_back(i.b, i.cost);
+            }
+        }
+    }
+    
+    GraphDijkstra (vector<Edge> edges, bool undir = false) {
+        this->pathcost = 0;
+        this->path.clear();
+        int n = 0;
+        for (auto&& i : edges) { n = max(max(i.a, i.b), n); }
+        this->n = n+1;
+        this->edgecost = vector< vector< pair<int, int> > >(this->n, vector< pair<int, int> >());
+        if (undir) {// undirected
+            for (auto&& i : edges) {
+                this->edgecost[i.a].emplace_back(i.b, i.cost);
+                this->edgecost[i.b].emplace_back(i.a, i.cost);
+            }
+        } else {// directed
+            for (auto&& i : edges) {
+                this->edgecost[i.a].emplace_back(i.b, i.cost);
+            }
+        }
+    }
+    
+    void calcPath (int start_i, int end_i) {
+        static const int limit = numeric_limits<int>::max();
+        this->path.clear();
+        this->rpath.clear();
+        if (this->tmp != start_i) {
+            this->prevs = vector<int>(this->n, -1);
+            this->costs = vector<int>(this->n, limit);
+            this->que = priority_queue<Node, vector<Node>, greater<Node> >();
+            this->costs[start_i] = 0;
+            this->que.emplace(0, start_i);
+        }
+        this->tmp = start_i;
+        
+        while (!this->que.empty()) {
+            const Node t = this->que.top();
+            if (t.cost >= this->costs[end_i]) { break; }
+            this->que.pop();
+            for (auto&& i : this->edgecost[t.index]) {
+                const int cost = t.cost + i.second;
+                if (cost < this->costs[i.first]) {
+                    this->costs[i.first] = cost;
+                    this->prevs[i.first] = t.index;
+                    this->que.emplace(cost, i.first);
+                }
+            }
+        }
+        
+        if (this->costs[end_i] < limit) {
+            this->pathcost = this->costs[end_i];
+            int e = end_i;
+            while (e > -1) {
+                this->rpath.emplace_back(e);
+                e = this->prevs[e];
+            }
+            this->path = this->rpath;
+            reverse(this->path.begin(), this->path.end());
+        } else {
+            this->pathcost = -1;
+        }
+    }
+};
+
+vector<Edge> gridToGraph (vector< vector<int> >& grid) {
+    static const int dx[4] = {0, 0, -1, 1};
+    static const int dy[4] = {-1, 1, 0, 0};
+    
+    vector<Edge> r;
+    int h = grid.size();
+    int w = grid[0].size();
+    for (int y = 0; y < h; ++y) {
+        for (int x = 0; x < w; ++x) {
+            int ax = x;
+            int ay = y;
+            for (int i = 0; i < 4; ++i) {
+                int bx = ax + dx[i];
+                int by = ay + dy[i];
+                if (bx < 0 || bx >= w || by < 0 || by >= h) { continue; }
+                r.emplace_back(Edge(ay*w+ax, by*w+bx, grid[by][bx]));
+            }
+        }
+    }
+    return r;
 }
 
 inline double distance (const double& ax, const double& ay, const double& bx, const double& by) {
